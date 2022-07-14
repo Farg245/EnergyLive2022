745b34b34bbb3dd28670e5ee632644793c414519
Ilias Paralikas <57923782+Ilias-Paralikas@users.noreply.github.com>
2022-07-14 13:44:02 +0300

    update button added

--- microservice07/app.js
+++ microservice07/app.js
@@ -23,147 +23,137 @@ const dbURI = 'mongodb+srv://User:IvfkezJoPM1JuCDC@saasproject.rqtkq.mongodb.net
 
 mongoose.connect(dbURI)
     .then((result) => {
-        app.listen(3000)
+        app.listen(7007)
     })
     .catch((err) => console.log(err))
 
 
+app.get('/',(req,res)=>{
 
-app.get('/', (req, res) => {
-    res.render('index')
-})
-
-app.get('/update_atl', async (req, res) => {
+    // update the data of atl
     fs.createReadStream("./csv_files/new_atl.csv")
-        .pipe(parse({ delimiter: "\t", from_line: 2 }))
-        .on("data", function (row) {
-            ATL.findOne({
-                DateTime: row[0],
-                ResolutionCode: row[1],
-                AreaCode: row[2],
-                AreaTypeCode: row[3],
-                AreaName: row[4],
-                MapCode: row[5],
-                TotalLoadValue: row[6],
-                UpdateTime: row[7]
-            },
+    .pipe(parse({ delimiter: "\t", from_line: 2 }))
+    .on("data", function (row) {
+        ATL.findOne({
+            DateTime: row[0],
+            ResolutionCode: row[1],
+            AreaCode: row[2],
+            AreaTypeCode: row[3],
+            AreaName: row[4],
+            MapCode: row[5],
+            TotalLoadValue: row[6],
+            UpdateTime: row[7]
+        },
 
-                function (err, data) {
-                    if (err) return handleError(err);
-                    if (data == null) {
-                        const atl = new ATL({
-                            _id: new mongoose.Types.ObjectId(),
-                            DateTime: row[0],
-                            ResolutionCode: row[1],
-                            AreaCode: row[2],
-                            AreaTypeCode: row[3],
-                            AreaName: row[4],
-                            MapCode: row[5],
-                            TotalLoadValue: row[6],
-                            UpdateTime: row[7]
-                        })
-                        atl.save()
-                    }
-                })
-        })
-    res.render('index')
-
-})
-
-
-app.get('/update_agpt', (req, res) => {
+            function (err, data) {
+                if (err) return handleError(err);
+                if (data == null) {
+                    const atl = new ATL({
+                        _id: new mongoose.Types.ObjectId(),
+                        DateTime: row[0],
+                        ResolutionCode: row[1],
+                        AreaCode: row[2],
+                        AreaTypeCode: row[3],
+                        AreaName: row[4],
+                        MapCode: row[5],
+                        TotalLoadValue: row[6],
+                        UpdateTime: row[7]
+                    })
+                    atl.save()
+                }
+            })
+    })
+    // update the data of agpt
 
     fs.createReadStream("./csv_files/new_agpt.csv")
-        .pipe(parse({ delimiter: "\t", from_line: 2 }))
-        .on("data", function (row) {
-            let generation = 0.0
-            if (row[7] != '') {
-                let generation = row[7]
-            }
-            let cons = 0.0
-            if (row[8] != '') {
-                let cons = row[8]
-            }
-            AGPT.findOne({
-                DateTime: row[0],
-                ResolutionCode: row[1],
-                AreaCode: row[2],
-                AreaTypeCode: row[3],
-                AreaName: row[4],
-                MapCode: row[5],
-                ProductionType: row[6],
-                ActualGenerationOutput: generation,
-                ActualConsumption: cons,
-                UpdateTime: row[9]
-            },
-                function (err, data) {
-                    if (err) return handleError(err);
-                    if (data == null) {
-                        const agpt = new AGPT({
-                            _id: new mongoose.Types.ObjectId(),
-                            DateTime: row[0],
-                            ResolutionCode: row[1],
-                            AreaCode: row[2],
-                            AreaTypeCode: row[3],
-                            AreaName: row[4],
-                            MapCode: row[5],
-                            ProductionType: row[6],
-                            ActualGenerationOutput: generation,
-                            ActualConsumption: cons,
-                            UpdateTime: row[9],
-                        })
-                        agpt.save()
-                    }
-                })
-        })
-    res.render('index')
+    .pipe(parse({ delimiter: "\t", from_line: 2 }))
+    .on("data", function (row) {
+        let generation = 0.0
+        if (row[7] != '') {
+            let generation = row[7]
+        }
+        let cons = 0.0
+        if (row[8] != '') {
+            let cons = row[8]
+        }
+        AGPT.findOne({
+            DateTime: row[0],
+            ResolutionCode: row[1],
+            AreaCode: row[2],
+            AreaTypeCode: row[3],
+            AreaName: row[4],
+            MapCode: row[5],
+            ProductionType: row[6],
+            ActualGenerationOutput: generation,
+            ActualConsumption: cons,
+            UpdateTime: row[9]
+        },
+            function (err, data) {
+                if (err) return handleError(err);
+                if (data == null) {
+                    const agpt = new AGPT({
+                        _id: new mongoose.Types.ObjectId(),
+                        DateTime: row[0],
+                        ResolutionCode: row[1],
+                        AreaCode: row[2],
+                        AreaTypeCode: row[3],
+                        AreaName: row[4],
+                        MapCode: row[5],
+                        ProductionType: row[6],
+                        ActualGenerationOutput: generation,
+                        ActualConsumption: cons,
+                        UpdateTime: row[9],
+                    })
+                    agpt.save()
+                }
+            })
+    })
+    // update the data of ff
 
-})
-
-
-
-app.get('/update_ff', (req, res) => {
     fs.createReadStream("./csv_files/new_ff.csv")
-        .pipe(parse({ delimiter: "\t", from_line: 2 }))
-        .on("data", function (row) {
-            FF.findOne({
-                DateTime: row[0],
-                ResolutionCode: row[1],
-                OutAreaCode: row[2],
-                OutAreaTypeCode: row[3],
-                OutAreaName: row[4],
-                OutMapCode: row[5],
-                InAreaCode: row[6],
-                InAreaTypeCode: row[7],
-                InAreaName: row[8],
-                InMapCode: row[9],
-                FlowValue: row[10],
-                UpdateTime: row[11]
-            },
-                function (err, data) {
-                    if (err) return handleError(err);
-                    if (data == null) {
-                        const ff = new FF({
-                            _id: new mongoose.Types.ObjectId(),
-                            DateTime: row[0],
-                            ResolutionCode: row[1],
-                            OutAreaCode: row[2],
-                            OutAreaTypeCode: row[3],
-                            OutAreaName: row[4],
-                            OutMapCode: row[5],
-                            InAreaCode: row[6],
-                            InAreaTypeCode: row[7],
-                            InAreaName: row[8],
-                            InMapCode: row[9],
-                            FlowValue: row[10],
-                            UpdateTime: row[11]
-                        })
-                        ff.save()
-                    }
-                })
-        })
-    res.render('index')
+    .pipe(parse({ delimiter: "\t", from_line: 2 }))
+    .on("data", function (row) {
+        FF.findOne({
+            DateTime: row[0],
+            ResolutionCode: row[1],
+            OutAreaCode: row[2],
+            OutAreaTypeCode: row[3],
+            OutAreaName: row[4],
+            OutMapCode: row[5],
+            InAreaCode: row[6],
+            InAreaTypeCode: row[7],
+            InAreaName: row[8],
+            InMapCode: row[9],
+            FlowValue: row[10],
+            UpdateTime: row[11]
+        },
+            function (err, data) {
+                if (err) return handleError(err);
+                if (data == null) {
+                    const ff = new FF({
+                        _id: new mongoose.Types.ObjectId(),
+                        DateTime: row[0],
+                        ResolutionCode: row[1],
+                        OutAreaCode: row[2],
+                        OutAreaTypeCode: row[3],
+                        OutAreaName: row[4],
+                        OutMapCode: row[5],
+                        InAreaCode: row[6],
+                        InAreaTypeCode: row[7],
+                        InAreaName: row[8],
+                        InMapCode: row[9],
+                        FlowValue: row[10],
+                        UpdateTime: row[11]
+                    })
+                    ff.save()
+                }
+            })
+    })
+    res.redirect("http://localhost:7002")
+
 
 })
+
+
 
 
